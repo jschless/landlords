8447ec2a62a884d14d98785d59a0c31497d8ec1a
jschless <joe.c.schlessinger@gmail.com>
2024-10-12 09:59:28 -0400

    player type hints

--- model/player.py
+++ model/player.py
@@ -11,7 +11,7 @@ class Player(BaseModel):
     exposed_cards: List[int] = Field(default_factory=list)
     spent_cards: List[int] = Field(default_factory=list)
 
-    def deal_cards(self, cards: List[int]):
+    def deal_cards(self, cards: List[int]) -> None:
         if len(cards) not in {3, 17}:
             raise ValueError(
                 "Received strange number of cards: should have gotten 17 or 3"
@@ -19,17 +19,17 @@ class Player(BaseModel):
         else:
             self.cards += cards
 
-    def flip_card(self, index_to_flip: int | None = None):
+    def flip_card(self, index_to_flip: int | None = None) -> None:
         if index_to_flip is None:
             self.exposed_cards.append(self.cards[random.randint(0, 16)])
         else:
             self.exposed_cards.append(self.cards[index_to_flip])
 
-    def make_landlord(self, cards: List[int]):
+    def make_landlord(self, cards: List[int]) -> None:
         self.cards += cards
         self.exposed_cards += cards
 
-    def remove_cards(self, cards: List[int]):
+    def remove_cards(self, cards: List[int]) -> None:
         for c in cards:
             self.cards.remove(c)
             if c in self.exposed_cards:
