edd9af2c28117ce5aafe630aa4abcd724a6ff2d9
jschless <joe.c.schlessinger@gmail.com>
2024-10-12 20:26:49 -0400

    Miscellaneous updates

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -7,19 +7,15 @@ import asyncio
 import httpx
 import uvicorn
 import multiprocessing
+from typing import List, Tuple
 
 
 def format_move(cards, kickers):
-    ans = {"action": "move"}
-    card_list = []
-    for c in cards:
-        card_list.append({"card": c})
-    kicker_list = []
-    for c in kickers:
-        kicker_list.append({"card": c})
-    ans["cards"] = card_list
-    ans["kickers"] = kicker_list
-    return ans
+    return {
+        "action": "move",
+        "cards": [{"card": c} for c in cards],
+        "kickers": [{"card": c} for c in kickers],
+    }
 
 
 def run_server():
@@ -31,62 +27,61 @@ def run_server():
 def fastapi_server():
     server_process = multiprocessing.Process(target=run_server)
     server_process.start()
-
     asyncio.run(asyncio.sleep(1))
-
-    yield  # This is where the tests will run
-
+    # This is where the tests will run
+    yield
     server_process.terminate()
     server_process.join()
 
 
-async def execute_moves(fastapi_server, p1_moves, p2_moves, p3_moves):
+async def execute_moves(fastapi_server, p1_moves, p2, p3):
     async with httpx.AsyncClient() as client:
         response = await client.post(f"http://localhost:8000/create_game")
         assert response.status_code == 200
         game_id = response.json()["game_id"]
 
-        async def connect_websocket(user_id, messages_to_send, delay):
-            await asyncio.sleep(delay)
-            async with websockets.connect(
-                f"ws://localhost:8000/ws/game/{game_id}?id={user_id}"
-            ) as websocket:
-                while True:
-                    try:
-                        message = await asyncio.wait_for(websocket.recv(), timeout=5)
-                        data = json.loads(message)
-                        if data["action"] in {
-                            "make_a_bid",
-                            "make_a_move",
-                            "game_over",
-                        }:
+    async def connect_websocket(user_id, messages_to_send, delay):
+        await asyncio.sleep(delay)
+        update = None
+        async with websockets.connect(
+            f"ws://localhost:8000/ws/game/{game_id}?id={user_id}"
+        ) as websocket:
+            while True:  # len(messages_to_send) > 0:
+                try:
+                    message = await asyncio.wait_for(websocket.recv(), timeout=5)
+                    data = json.loads(message)
+                    if data["action"] in {
+                        "make_a_bid",
+                        "make_a_move",
+                        "game_over",
+                    }:
+                        if len(messages_to_send) > 0:
                             response = messages_to_send.pop()
                             await websocket.send(json.dumps(response))
-                        elif data["action"] == "update":
-                            update = data
+                    elif data["action"] == "update":
+                        update = data
 
-                    except asyncio.TimeoutError:
-                        return update
-                    except Exception as e:
-                        return update
+                except asyncio.TimeoutError:
+                    return update
+            return update
 
-        tasks = [
-            connect_websocket("Tom", p1_moves[::-1], 0),
-            connect_websocket("Dick", p2_moves[::-1], 1),
-            connect_websocket("Harry", p3_moves[::-1], 2),
-        ]
-        results = await asyncio.gather(*tasks)
-        return results
+    tasks = [
+        connect_websocket("Tom", p1_moves[::-1], 0),
+        connect_websocket("Dick", p2[::-1], 1),
+        connect_websocket("Harry", p3[::-1], 2),
+    ]
+    results = await asyncio.gather(*tasks)
+    return results
 
 
 @pytest.mark.asyncio
 async def test_through_bet(fastapi_server):
-    p1_moves = [{"action": "bet", "bet": "1"}]
-    p2_moves = [{"action": "bet", "bet": "2"}]
-    p3_moves = [{"action": "bet", "bet": "3"}]
+    p1 = [{"action": "bet", "bet": "1"}]
+    p2 = [{"action": "bet", "bet": "2"}]
+    p3 = [{"action": "bet", "bet": "3"}]
 
-    results = await execute_moves(fastapi_server, p1_moves, p2_moves, p3_moves)
-    harry = next((x for x in results if x["username"] == "Harry_U"), None)
+    results = await execute_moves(fastapi_server, p1, p2, p3)
+    tom, dick, harry = tuple(results)
 
     assert harry["landlord"] == 2
     assert len(harry["my_cards"]) == 20
@@ -94,23 +89,18 @@ async def test_through_bet(fastapi_server):
 
 @pytest.mark.asyncio
 async def test_through_first_move(fastapi_server):
-    p1_moves = [
-        {"action": "bet", "bet": "3"},
-        {"action": "move", "cards": [{"card": 3}, {"card": 3}], "kickers": []},
-    ]
-    p2_moves = [
-        {"action": "move", "cards": [{"card": 7}, {"card": 7}], "kickers": []},
-    ]
-    p3_moves = [
-        {"action": "move", "cards": [{"card": 11}, {"card": 11}], "kickers": []},
-    ]
+    p1 = [{"action": "bet", "bet": "3"}, format_move([3, 3], [])]
+    p2 = [format_move([7, 7], [])]
+    p3 = [format_move([11, 11], [])]
 
-    results = await execute_moves(fastapi_server, p1_moves, p2_moves, p3_moves)
-    tom = next((x for x in results if x["username"] == "Tom_U"), None)
+    results = await execute_moves(fastapi_server, p1, p2, p3)
+
+    tom, dick, harry = tuple(results)
+
     assert tom["my_cards"].count(3) == 0
     assert len(tom["my_cards"]) == 18
     for p in tom["players"]:
-        if p["username"] == "Harry_U":
+        if p["username"] == harry["username"]:
             # Landlord has played two cards (20 - 18)
             assert p["n_cards"] == 15
         else:
@@ -125,64 +115,66 @@ async def test_complete_hand(fastapi_server):
     P1 wins with a better hand.
     Everyone passes until P1 wins
     """
-    p1_moves = [
+    p1 = [
         {"action": "bet", "bet": "1"},
         format_move([6, 7, 8, 9, 10, 11], []),
     ]
-    p2_moves = [{"action": "bet", "bet": "2"}, format_move([], [])]
-    p3_moves = [
+    p2 = [{"action": "bet", "bet": "2"}, format_move([], [])]
+    p3 = [
         {"action": "bet", "bet": "3"},
         format_move([3, 4, 5, 6, 7, 8], []),
         format_move([], []),
     ]
 
-    results = await execute_moves(fastapi_server, p1_moves, p2_moves, p3_moves)
-    tom = next((x for x in results if x["username"] == "Tom_U"), None)
+    results = await execute_moves(fastapi_server, p1, p2, p3)
+
+    tom, dick, harry = tuple(results)
     assert len(tom["my_cards"]) == 11
     assert tom["current_player"] == 0
 
 
 @pytest.mark.asyncio
 async def test_complete_round(fastapi_server):
-    p1_moves = [
+    p1 = [
         {"action": "bet", "bet": "1"},
         format_move([], []),
         format_move([], []),
     ]
-    p2_moves = [
+    p2 = [
         {"action": "bet", "bet": "2"},
         format_move([], []),
         format_move([], []),
     ]
-    p3_moves = [
+    p3 = [
         {"action": "bet", "bet": "3"},
         format_move([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], []),
         format_move([11, 12, 13, 14, 15], []),
         format_move([16, 17], []),
     ]
 
-    results = await execute_moves(fastapi_server, p1_moves, p2_moves, p3_moves)
-    harry = next((x for x in results if x["username"] == "Harry_U"), None)
+    results = await execute_moves(fastapi_server, p1, p2, p3)
+
+    tom, dick, harry = tuple(results)
+
     assert len(harry["my_cards"]) == 0
-    tom = next((x for x in results if x["username"] == "Tom_U"), None)
     assert len(tom["my_cards"]) == 17
 
-    assert harry["scoreboard"]["Harry_U"] == 12
+    assert harry["scoreboard"][harry["username"]] == 12
 
 
 @pytest.mark.asyncio
 async def test_two_rounds(fastapi_server):
-    p1_moves = [
+    p1 = [
         {"action": "bet", "bet": "1"},
         format_move([], []),
         format_move([], []),
     ] * 2
-    p2_moves = [
+    p2 = [
         {"action": "bet", "bet": "2"},
         format_move([], []),
         format_move([], []),
     ] * 2
-    p3_moves = [
+    p3 = [
         {"action": "bet", "bet": "3"},
         format_move([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], []),
         format_move([11, 12, 13, 14, 15], []),
@@ -190,9 +182,9 @@ async def test_two_rounds(fastapi_server):
         {"action": "play_again", "decision": True},
     ] * 2
 
-    results = await execute_moves(fastapi_server, p1_moves, p2_moves, p3_moves)
-    harry = next((x for x in results if x["username"] == "Harry_U"), None)
-    assert harry["scoreboard"]["Harry_U"] == 24
+    results = await execute_moves(fastapi_server, p1, p2, p3)
+    tom, dick, harry = tuple(results)
+    assert harry["scoreboard"][harry["username"]] == 24
     # assert len(harry["my_cards"]) == 0
     # tom = next((x for x in results if x["username"] == "Tom_U"), None)
     # assert len(tom["my_cards"]) == 17
@@ -200,31 +192,29 @@ async def test_two_rounds(fastapi_server):
 
 @pytest.mark.asyncio
 async def test_invalid_start_move(fastapi_server):
-    p1_moves = [
+    p1 = [
         {"action": "bet", "bet": "1"},
         format_move([6, 7, 8, 9, 10], []),
         format_move([], []),
     ]
-    p2_moves = [
+    p2 = [
         {"action": "bet", "bet": "2"},
         format_move([], []),
         format_move([], []),
     ]
-    p3_moves = [
+    p3 = [
         {"action": "bet", "bet": "3"},
         format_move([3, 5, 6, 7, 8], []),  # bad move
         format_move([3, 4, 5, 6, 7], []),  # correct move
         format_move([], []),
     ]
 
-    results = await execute_moves(fastapi_server, p1_moves, p2_moves, p3_moves)
+    results = await execute_moves(fastapi_server, p1, p2, p3)
 
     # So Tom should be current_play
-    assert results[0]["current_player"] == 0
 
-    harry = next((x for x in results if x["username"] == "Harry_U"), None)
-    tom = next((x for x in results if x["username"] == "Tom_U"), None)
-    dick = next((x for x in results if x["username"] == "Dick_U"), None)
+    tom, dick, harry = tuple(results)
+    assert tom["current_player"] == 0
     assert len(harry["my_cards"]) == 15
     assert len(tom["my_cards"]) == 12
     assert len(dick["my_cards"]) == 17
@@ -232,33 +222,36 @@ async def test_invalid_start_move(fastapi_server):
 
 @pytest.mark.asyncio
 async def test_invalid_following_move(fastapi_server):
-    p1_moves = [
+    p1 = [
         {"action": "bet", "bet": "1"},
         format_move([6, 7, 8, 9, 10, 11], []),
         format_move([], []),
     ]
-    p2_moves = [
+    p2 = [
         {"action": "bet", "bet": "2"},
         format_move([], []),
         format_move([], []),
     ]
-    p3_moves = [
+    p3 = [
         {"action": "bet", "bet": "3"},
         format_move([3, 5, 6, 7, 8], []),  # bad move
         format_move([3, 4, 5, 6, 7], []),  # correct move
         format_move([], []),
     ]
 
-    results = await execute_moves(fastapi_server, p1_moves, p2_moves, p3_moves)
+    results = await execute_moves(fastapi_server, p1, p2, p3)
 
-    assert results[0]["current_player"] == 2
+    tom, dick, harry = tuple(results)
+    assert tom["current_player"] == 2
 
-    harry = next((x for x in results if x["username"] == "Harry_U"), None)
-    tom = next((x for x in results if x["username"] == "Tom_U"), None)
-    dick = next((x for x in results if x["username"] == "Dick_U"), None)
     assert len(harry["my_cards"]) == 15
     assert len(tom["my_cards"]) == 17
     assert len(dick["my_cards"]) == 17
+
+
+@pytest.mark.asyncio
+async def test_simultaneous_connections():
+    pass
 
 
 @pytest.mark.asyncio
