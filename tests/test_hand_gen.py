a83807e8f27d3379e786c436d7f3edd17cd32505
jschless <joe.c.schlessinger@gmail.com>
2024-10-14 18:08:56 -0400

    simplify code with parameters

--- tests/test_hand_gen.py
+++ tests/test_hand_gen.py
@@ -2,185 +2,122 @@ from model.hand import Hand
 import pytest
 
 
-def test_pairs():
-    hand = [3, 4, 5, 5, 6, 7, 7, 7, 9, 9, 9, 11, 11, 12, 14, 15, 15]
-    hand_cards = [6, 6]
-    kicker_cards = []
+@pytest.mark.parametrize(
+    "hand, hand_cards, kicker_cards, expected_moves",
+    [
+        # Test for pairs
+        (
+            [3, 4, 5, 5, 6, 7, 7, 7, 9, 9, 9, 11, 11, 12, 14, 15, 15],
+            [6, 6],
+            [],
+            [([7, 7], []), ([9, 9], []), ([11, 11], []), ([15, 15], [])],
+        ),
+        # Test for triples
+        (
+            [3, 4, 5, 5, 6, 7, 7, 7, 9, 9, 9, 11, 11, 12, 14, 15, 15],
+            [8, 8, 8],
+            [],
+            [([9, 9, 9], [])],
+        ),
+        # Test for pair chains
+        (
+            [3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 9, 9, 9, 11, 11, 12, 14, 15, 15],
+            [3, 3, 4, 4, 5, 5],
+            [],
+            [([4, 4, 5, 5, 6, 6], []), ([5, 5, 6, 6, 7, 7], [])],
+        ),
+        # Test for triple chains
+        (
+            [3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 11, 11, 12, 14, 15, 15],
+            [4, 4, 4, 5, 5, 5],
+            [],
+            [([7, 7, 7, 8, 8, 8], []), ([8, 8, 8, 9, 9, 9], [])],
+        ),
+        # Test for triple single discard
+        (
+            [3, 5, 5, 5, 7, 7, 7, 9, 9, 9],
+            [6, 6, 6],
+            [4],
+            [
+                ([7, 7, 7], [3]),
+                ([7, 7, 7], [5]),
+                ([7, 7, 7], [9]),
+                ([9, 9, 9], [3]),
+                ([9, 9, 9], [5]),
+                ([9, 9, 9], [7]),
+            ],
+        ),
+        # Test for triple pair discard
+        (
+            [3, 5, 5, 5, 7, 7, 7, 9, 9, 9],
+            [6, 6, 6],
+            [4, 4],
+            [
+                ([7, 7, 7], [5, 5]),
+                ([7, 7, 7], [9, 9]),
+                ([9, 9, 9], [5, 5]),
+                ([9, 9, 9], [7, 7]),
+            ],
+        ),
+        # Test for airplane single discard
+        (
+            [3, 5, 5, 5, 7, 7, 7, 8, 8, 8],
+            [6, 6, 6, 7, 7, 7],
+            [4, 7],
+            [
+                ([7, 7, 7, 8, 8, 8], [3, 5]),
+            ],
+        ),
+        # Test for airplane dual pair discard
+        (
+            [3, 3, 4, 5, 5, 5, 7, 7, 7, 8, 8, 8],
+            [6, 6, 6, 7, 7, 7],
+            [4, 4, 7, 7],
+            [
+                ([7, 7, 7, 8, 8, 8], [3, 3, 5, 5]),
+            ],
+        ),
+        # Test for bomb single discard
+        (
+            [3, 3, 4, 5, 5, 5, 5, 8, 8, 8, 8],
+            [6, 6, 6, 6],
+            [4, 5],
+            [
+                ([8, 8, 8, 8], [3, 4]),
+                ([8, 8, 8, 8], [3, 5]),
+                ([8, 8, 8, 8], [4, 5]),
+            ],
+        ),
+        # Test for bomb dual discard
+        (
+            [3, 3, 4, 5, 5, 5, 5, 8, 8, 8, 8, 10, 10],
+            [6, 6, 6, 6],
+            [4, 4, 5, 5],
+            [
+                ([8, 8, 8, 8], [3, 3, 5, 5]),
+                ([8, 8, 8, 8], [3, 3, 10, 10]),
+                ([8, 8, 8, 8], [5, 5, 10, 10]),
+            ],
+        ),
+        # Test for always ultimate bomb
+        (
+            [3, 3, 4, 5, 5, 5, 5, 8, 8, 8, 8, 10, 10, 16, 17],
+            [6, 6, 6, 6],
+            [4, 4, 5, 5],
+            [
+                ([8, 8, 8, 8], [3, 3, 5, 5]),
+                ([8, 8, 8, 8], [3, 3, 10, 10]),
+                ([8, 8, 8, 8], [5, 5, 10, 10]),
+                ([16, 17], []),
+            ],
+        ),
+        # Test for no moves
+        ([3, 3, 4, 5, 5, 5, 5, 8, 8, 8, 10, 10, 16], [6, 6, 6, 6], [4, 4, 5, 5], []),
+    ],
+)
+def test_possible_hands(hand, hand_cards, kicker_cards, expected_moves):
     h = Hand.parse_hand(hand_cards, kicker_cards)
     moves = h.possible_hands(hand)
-    assert moves == [
-        ([7, 7], []),
-        ([9, 9], []),
-        ([11, 11], []),
-        ([15, 15], []),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_triples():
-    hand = [3, 4, 5, 5, 6, 7, 7, 7, 9, 9, 9, 11, 11, 12, 14, 15, 15]
-    hand_cards = [8, 8, 8]
-    kicker_cards = []
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([9, 9, 9], []),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_pair_chains():
-    hand = [3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 9, 9, 9, 11, 11, 12, 14, 15, 15]
-    hand_cards = [3, 3, 4, 4, 5, 5]
-    kicker_cards = []
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([4, 4, 5, 5, 6, 6], []),
-        ([5, 5, 6, 6, 7, 7], []),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_triple_chains():
-    hand = [3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 11, 11, 12, 14, 15, 15]
-    hand_cards = [4, 4, 4, 5, 5, 5]
-    kicker_cards = []
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([7, 7, 7, 8, 8, 8], []),
-        ([8, 8, 8, 9, 9, 9], []),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_triple_single_discard():
-    hand = [3, 5, 5, 5, 7, 7, 7, 9, 9, 9]
-    hand_cards = [6, 6, 6]
-    kicker_cards = [4]
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([7, 7, 7], [3]),
-        ([7, 7, 7], [5]),
-        ([7, 7, 7], [9]),
-        ([9, 9, 9], [3]),
-        ([9, 9, 9], [5]),
-        ([9, 9, 9], [7]),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_triple_pair_discard():
-    hand = [3, 5, 5, 5, 7, 7, 7, 9, 9, 9]
-    hand_cards = [6, 6, 6]
-    kicker_cards = [4, 4]
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([7, 7, 7], [5, 5]),
-        ([7, 7, 7], [9, 9]),
-        ([9, 9, 9], [5, 5]),
-        ([9, 9, 9], [7, 7]),
-    ]
-
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_airplane_single_discard():
-    hand = [3, 5, 5, 5, 7, 7, 7, 8, 8, 8]
-    hand_cards = [6, 6, 6, 7, 7, 7]
-    kicker_cards = [
-        4,
-        7,
-    ]
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([7, 7, 7, 8, 8, 8], [3, 5]),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_airplane_dual_pair_discard():
-    hand = [3, 3, 4, 5, 5, 5, 7, 7, 7, 8, 8, 8]
-    hand_cards = [6, 6, 6, 7, 7, 7]
-    kicker_cards = [
-        4,
-        4,
-        7,
-        7,
-    ]
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([7, 7, 7, 8, 8, 8], [3, 3, 5, 5]),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_bomb_single_discard():
-    hand = [3, 3, 4, 5, 5, 5, 5, 8, 8, 8, 8]
-    hand_cards = [6, 6, 6, 6]
-    kicker_cards = [4, 5]
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([8, 8, 8, 8], [3, 4]),
-        ([8, 8, 8, 8], [3, 5]),
-        ([8, 8, 8, 8], [4, 5]),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_bomb_dual_discard():
-    hand = [3, 3, 4, 5, 5, 5, 5, 8, 8, 8, 8, 10, 10]
-    hand_cards = [6, 6, 6, 6]
-    kicker_cards = [4, 4, 5, 5]
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == [
-        ([8, 8, 8, 8], [3, 3, 5, 5]),
-        ([8, 8, 8, 8], [3, 3, 10, 10]),
-        ([8, 8, 8, 8], [5, 5, 10, 10]),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_always_ultimate_bomb():
-    hand = [3, 3, 4, 5, 5, 5, 5, 8, 8, 8, 8, 10, 10, 16, 17]
-    hand_cards = [6, 6, 6, 6]
-    kicker_cards = [4, 4, 5, 5]
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-
-    assert moves == [
-        ([8, 8, 8, 8], [3, 3, 5, 5]),
-        ([8, 8, 8, 8], [3, 3, 10, 10]),
-        ([8, 8, 8, 8], [5, 5, 10, 10]),
-        ([16, 17], []),
-    ]
-    for a, b in moves:
-        assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
-
-
-def test_no_moves():
-    hand = [3, 3, 4, 5, 5, 5, 5, 8, 8, 8, 10, 10, 16]
-    hand_cards = [6, 6, 6, 6]
-    kicker_cards = [4, 4, 5, 5]
-    h = Hand.parse_hand(hand_cards, kicker_cards)
-    moves = h.possible_hands(hand)
-    assert moves == []
+    assert moves == expected_moves
     for a, b in moves:
         assert h.is_valid_successor(Hand.parse_hand(a, b)) == True
