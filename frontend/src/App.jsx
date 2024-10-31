929e16430fe55ffdfa8a92917f867941479902b2
jschless <joe.c.schlessinger@gmail.com>
2024-10-30 20:36:29 -0400

    custom 404 errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import GameLobby from "./GameLobby";
+import NotFound from "./NotFound";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game/:id" element={<GameLobby />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </ChakraProvider>
