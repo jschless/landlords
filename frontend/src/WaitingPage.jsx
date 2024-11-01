b596ebd3d2a2697fe6d37d539fb239bbb48fcc92
jschless <joe.c.schlessinger@gmail.com>
2024-11-01 19:27:44 -0400

    spruced up waiting page

--- frontend/src/WaitingPage.jsx
+++ frontend/src/WaitingPage.jsx
@@ -8,36 +8,61 @@ import {
   Container,
   Stack,
   useColorModeValue,
+  IconButton,
+  useClipboard,
+  Tooltip,
+  HStack,
 } from "@chakra-ui/react";
 
+import { CopyIcon } from "@chakra-ui/icons";
+
 function WaitingPage({ gameData }) {
+  const currentUrl = window.location.href;
+  const { hasCopied, onCopy } = useClipboard(currentUrl);
+
   return (
     <Container maxW="container.sm" p={6} centerContent>
       <Stack spacing={6} align="center" textAlign="center">
-        <Heading as="h1" size="xl">
-          Game Lobby: {gameData.game_id}
-        </Heading>
+        <Heading size="lg">Dough Dizhu Lobby</Heading>
+        <Heading size="sm">Game ID: {gameData.game_id}</Heading>
+
+        <HStack spacing={4} align="center">
+          <Text fontSize="lg" color={useColorModeValue("gray.600", "gray.300")}>
+            Click to copy and share the link with a friend!
+          </Text>
+          <Tooltip label={hasCopied ? "Copied!" : "Copy link"} fontSize="md">
+            <IconButton
+              icon={<CopyIcon />}
+              onClick={onCopy}
+              aria-label="Copy game link"
+              variant="outline"
+              colorScheme="teal"
+            />
+          </Tooltip>
+        </HStack>
+
         <Text fontSize="lg" color={useColorModeValue("gray.600", "gray.300")}>
           Waiting for {2 - gameData.players.length} player
-          {gameData.players.length === 1 ? "" : "s"} to join. Here's who's
+          {gameData.players.length === 1 ? "" : "s"} to join. Here’s who’s
           currently here:
         </Text>
+
         <Box
-          borderWidth={1}
+          borderWidth={2}
           borderRadius="md"
-          borderColor={useColorModeValue("gray.200", "gray.700")}
+          borderColor={useColorModeValue("gray.300", "gray.600")}
           p={4}
-          bg={useColorModeValue("white", "gray.800")}
+          bg={useColorModeValue("white", "gray.900")}
           width="full"
+          boxShadow="lg"
         >
           <List spacing={3} textAlign="left">
-            <ListItem>
-              {/* <Text fontWeight="bold">You:</Text> */}
-              {gameData.username}
+            <ListItem fontWeight="bold">
+              1. {gameData.username} (you)
             </ListItem>
             {gameData.players.map((player, index) => (
-              <ListItem key={index} pl={4}>
-                {player.username}
+              <ListItem key={index} fontWeight="medium">
+                {index + 2}. {player.username}
               </ListItem>
             ))}
           </List>
