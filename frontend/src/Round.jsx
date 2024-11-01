f348470b5b12ae9b06ac47db7452377d0eb1b78f
jschless <joe.c.schlessinger@gmail.com>
2024-11-01 14:42:55 -0400

    made round better

--- frontend/src/Round.jsx
+++ frontend/src/Round.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Box, Flex, Image, Text, Heading } from "@chakra-ui/react";
 
 const Round = ({ hands, bidValue, currentPlayer }) => {
+  const scrollRef = useRef();
+
+  useEffect(() => {
+    // Scroll to the bottom of the card list when hands change
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [hands]);
+
   return (
     <Box
       textAlign="center"
@@ -11,60 +20,65 @@ const Round = ({ hands, bidValue, currentPlayer }) => {
       shadow="md"
       flex="3"
     >
-      <Flex direction="column" alignItems="center" gap="4">
-        <Heading>Playing Area</Heading>
+      <Heading>Playing Area</Heading>
 
-        {hands.map(([player, hand], index) => (
-          <Flex key={index} alignItems="center" mb={4}>
-            <Text p={0} m={2}>
-              ({index + 1}) {player}
-            </Text>
-            <Flex
-              direction="column"
-              alignItems="center"
-              borderWidth={1}
-              borderColor="gray.300"
-              borderRadius="md"
-              p={2}
-              mr={4}
-            >
-              <Text fontWeight="bold" mb={2}>
-                Hand
+      {/* Scrollable area for hands */}
+      <Box
+        ref={scrollRef}
+        mt={4}
+        maxHeight="700px"  // Set max height for the scrollable area
+        overflowY="auto"
+        scrollBehavior="smooth"
+        p={2}
+      >
+        <Flex direction="column" alignItems="center" gap="4">
+          {hands.map(([player, hand], index) => (
+            <Flex key={index} alignItems="center" mb={4}>
+              <Text p={0} m={2}>
+                ({index + 1}) {player}
               </Text>
-              {hand ? ( // Check if hand is not null
-                <Flex direction="row" justifyContent="center">
-                  {hand.hand_cards.map((card, i) => (
-                    <Box
-                      key={i}
-                      p={1}
-                      borderWidth={1}
-                      borderColor="black"
-                      borderRadius="md"
-                      mx={1}
-                      transition="transform 0.2s"
-                      _hover={{ transform: "scale(1.1)" }}
-                    >
-                      <Image
-                        src={`${process.env.PUBLIC_URL}/cards/${card}.png`}
-                        alt={`Card ${card}`}
-                        boxSize="50px"
-                        objectFit="cover"
-                      />
-                    </Box>
-                  ))}
-                </Flex>
-              ) : (
-                <Text fontWeight="bold" color="red.500">
-                  {" "}
-                  {/* Display "Pass" if hand is null */}
-                  Pass
+              <Flex
+                direction="column"
+                alignItems="center"
+                borderWidth={1}
+                borderColor="gray.300"
+                borderRadius="md"
+                p={2}
+                mr={4}
+              >
+                <Text fontWeight="bold" mb={2}>
+                  Hand
                 </Text>
-              )}
-            </Flex>
-            <Box width="20px" /> {/* Spacer */}
-            {/* Conditionally render the kicker section if there are kicker cards */}
-            {hand &&
-              hand.kicker_cards.length > 0 && ( // Check if hand is not null before rendering kicker cards
+                {hand ? (
+                  <Flex direction="row" justifyContent="center">
+                    {hand.hand_cards.map((card, i) => (
+                      <Box
+                        key={i}
+                        p={1}
+                        borderWidth={1}
+                        borderColor="black"
+                        borderRadius="md"
+                        mx={1}
+                        transition="transform 0.2s"
+                        _hover={{ transform: "scale(1.1)" }}
+                      >
+                        <Image
+                          src={`${process.env.PUBLIC_URL}/cards/${card}.png`}
+                          alt={`Card ${card}`}
+                          boxSize="50px"
+                          objectFit="cover"
+                        />
+                      </Box>
+                    ))}
+                  </Flex>
+                ) : (
+                  <Text fontWeight="bold" color="red.500">
+                    Pass
+                  </Text>
+                )}
+              </Flex>
+              <Box width="20px" /> {/* Spacer */}
+              {hand && hand.kicker_cards.length > 0 && (
                 <Flex
                   direction="column"
                   alignItems="center"
@@ -99,9 +113,10 @@ const Round = ({ hands, bidValue, currentPlayer }) => {
                   </Flex>
                 </Flex>
               )}
-          </Flex>
-        ))}
-      </Flex>
+            </Flex>
+          ))}
+        </Flex>
+      </Box>
     </Box>
   );
 };
