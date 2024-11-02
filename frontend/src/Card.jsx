1afd832204d2b596d9e10a5a1e2ee9e7647a16a3
jschless <joe.c.schlessinger@gmail.com>
2024-11-01 20:27:38 -0400

    missed one instance

---
+++ frontend/src/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Image, Box } from "@chakra-ui/react";
+
+function Card({ index, card, border, onClick }) {
+  return (
+    <Box transition="transform 0.2s" _hover={{ transform: "scale(1.1)" }}>
+      <Image
+        src={`${process.env.PUBLIC_URL}/cards/${card}.png`}
+        alt={`Card ${card}`}
+        boxSize="60px"
+        m={2}
+        border={border}
+        cursor="pointer"
+        onClick={onClick}
+      />
+    </Box>
+  );
+}
+
+export default Card;
--- frontend/src/RoundInfo.jsx
+++ frontend/src/RoundInfo.jsx
@@ -9,6 +9,7 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { motion, useAnimation } from "framer-motion";
+import Card from "./Card.jsx";
 
 const MotionText = motion.create(Text);
 
@@ -56,23 +57,7 @@ function RoundInfo({ gameData }) {
           </Heading>
           <Flex justify="center" gap={2}>
             {lastHand?.hand_cards?.map((card, i) => (
-              <Box
-                key={i}
-                p={1}
-                borderWidth={1}
-                borderColor="black"
-                borderRadius="md"
-                mx={1}
-                transition="transform 0.2s"
-                _hover={{ transform: "scale(1.1)" }}
-              >
-                <Image
-                  src={`${process.env.PUBLIC_URL}/cards/${card}.png`}
-                  alt={`Card ${card}`}
-                  boxSize="50px"
-                  objectFit="cover"
-                />
-              </Box>
+                <Card key={i} card={card}/>
             ))}
           </Flex>
         </Box>
